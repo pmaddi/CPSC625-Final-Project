17f7d49e825c42e2e5bbf0b1711b38be578f0227
Pranav Maddi <pranav.maddi@gmail.com>
2015-12-10 13:28:31 -0500

    sdf'

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-
-for ratio in 0 .25 .5 .75 1.0
+for i in {1..20}
 do
-    rm zookeeper.db;
-    ~/anaconda/bin/python zookeeper_server.py &> log_$1.log &
-    sleep 1;
-    ~/anaconda/bin/python experiment.py $2 | tee -a log_exp_$1.log &&
+    for ratio in 0 .1 .2 .3 .4 .5 .6 .7 .8 .9 1.0
+    do
+        rm zookeeper.db;
+        ~/anaconda/bin/python zookeeper_server.py &> log_$1.log &
+        sleep 1;
+        ~/anaconda/bin/python experiment.py $ratio | tee -a log_exp_$1.log &&
+        kill $(jobs -pr);
+    done
     trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT;
 done
 printf \\a
